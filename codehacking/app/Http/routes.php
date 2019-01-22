01ea556cfa767f05134fb5bd7f21bd6de2fa04ff
imzoughene <imzoughene.youssef.01@gmail.com>
2019-01-22 13:36:15 +0100

    get admin layout

--- codehacking/app/Http/routes.php
+++ codehacking/app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
